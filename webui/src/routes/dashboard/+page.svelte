9f5028a5987a2433463b5428dbf0c2c97220aa25
giomara-larraga <giomara.lm@gmail.com>
2025-05-28 15:10:00 +0300

    (GUI) Update Topbar link and enhance dashboard layout with cards

--- webui/src/routes/dashboard/+page.svelte
+++ webui/src/routes/dashboard/+page.svelte
@@ -1,13 +1,112 @@
 <script lang="ts">
 	import Topbar from '$lib/components/ui/topbar/Topbar.svelte';
+	import CircleUser from '@lucide/svelte/icons/user-circle';
+	import Method from '@lucide/svelte/icons/brain-circuit';
+	import Logo from '@lucide/svelte/icons/orbit';
+	import Problem from '@lucide/svelte/icons/puzzle';
+	import Archive from '@lucide/svelte/icons/archive';
+	import HelpCircle from '@lucide/svelte/icons/circle-help';
+	import * as Card from "$lib/components/ui/card/index.js";
+	import { Button } from '$lib/components/ui/button/index.js';
+
+
 </script>
 
 <div class="flex min-h-screen w-full flex-col">
 	<Topbar />
 
 	<main
-		class="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-teal-50 bg-white p-4 md:gap-8 md:p-10"
+		class="mx-auto flex max-w-5xl flex-col items-center gap-6 text-center"
 	>
-		This is an example of how to use the Topbar in a layout.
+		<h1 class="mb-6 text-4xl font-semibold text-pretty lg:text-5xl text-primary pt-4">Welcome to DESDEO</h1>
+		<p class="text-lg text-gray-700 text-center">
+			Explore the features and functionalities to enhance your decision-making process.
+		</p>
+		<div class="mt-10 grid grid-cols-1 place-items-center gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            <Card.Root >
+              <Card.Header class="pb-1">
+                <Problem class="size-4" strokeWidth={1} />
+              </Card.Header>
+              <Card.Content class="text-left">
+                <h2 class="mb-1 text-lg font-semibold">Card Title</h2>
+                <p class="leading-snug text-muted-foreground">
+                  Lorem ipsum dolor sit amet consectetur.
+                </p>
+              </Card.Content>
+              <Card.Footer>
+                <Button class="w-full">Do something</Button>
+              </Card.Footer>
+            </Card.Root>
+            <Card.Root >
+              <Card.Header class="pb-1">
+                <Method class="size-4" strokeWidth={1} />
+              </Card.Header>
+              <Card.Content class="text-left">
+                <h2 class="mb-1 text-lg font-semibold">Card Title</h2>
+                <p class="leading-snug text-muted-foreground">
+                  Lorem ipsum dolor sit amet consectetur.
+                </p>
+              </Card.Content>
+              <Card.Footer >
+				<Button class="w-full">Do something</Button>
+              </Card.Footer>
+            </Card.Root>
+            <Card.Root >
+              <Card.Header class="pb-1">
+                <Archive class="size-4" strokeWidth={1} />
+              </Card.Header>
+              <Card.Content class="text-left">
+                <h2 class="mb-1 text-lg font-semibold">Card Title</h2>
+                <p class="leading-snug text-muted-foreground">
+                  Lorem ipsum dolor sit amet consectetur.
+                </p>
+              </Card.Content>
+              <Card.Footer >
+				<Button class="w-full">Do something</Button>
+              </Card.Footer>
+            </Card.Root>
+            <Card.Root >
+              <Card.Header class="pb-1">
+                <HelpCircle class="size-4" strokeWidth={1} />
+              </Card.Header>
+              <Card.Content class="text-left">
+                <h2 class="mb-1 text-lg font-semibold">Card Title</h2>
+                <p class="leading-snug text-muted-foreground">
+                  Lorem ipsum dolor sit amet consectetur.
+                </p>
+              </Card.Content>
+              <Card.Footer >
+				<Button class="w-full">Do something</Button>
+              </Card.Footer>
+            </Card.Root>
+            <Card.Root >
+              <Card.Header class="pb-1">
+                <CircleUser class="size-4" strokeWidth={1} />
+              </Card.Header>
+              <Card.Content class="text-left">
+                <h2 class="mb-1 text-lg font-semibold">Card Title</h2>
+                <p class="leading-snug text-muted-foreground">
+                  Lorem ipsum dolor sit amet consectetur.
+                </p>
+              </Card.Content>
+              <Card.Footer >
+				<Button class="w-full">Do something</Button>
+              </Card.Footer>
+            </Card.Root>
+            <Card.Root >
+              <Card.Header class="pb-1">
+                <Problem class="size-4" strokeWidth={1} />
+              </Card.Header>
+              <Card.Content class="text-left">
+                <h2 class="mb-1 text-lg font-semibold">Card Title</h2>
+                <p class="leading-snug text-muted-foreground">
+                  Lorem ipsum dolor sit amet consectetur.
+                </p>
+              </Card.Content>
+              <Card.Footer >
+				<Button class="w-full">Do something</Button>
+              </Card.Footer>
+            </Card.Root>
+          </div>
 	</main>
 </div>
